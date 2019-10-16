d18a12b785c227504d9542474cf2aedc8a07e5fa
vicky <vikrantsolanki817@gmail.com>
2019-10-17 01:18:23 +0530

    level01

---
+++ app/src/main/java/com/example/smackchat/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.smackchat
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import android.view.Menu
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+
+
+
+    }
+
+    fun loginbtnNavClicked(view: View) {
+
+    }
+
+    fun addchannelclicked(view: View){
+
+    }
+    fun sendmsgbtnclicked(view: View){
+        
+    }
+}
