7347b3df6932b9ea68227cf064281972582acfdd
vicky <vikrantsolanki817@gmail.com>
2019-11-09 15:21:36 +0530

    last

--- app/src/main/java/com/example/smackchat/Utilities/Constants.kt
+++ app/src/main/java/com/example/smackchat/Utilities/Constants.kt
@@ -2,14 +2,16 @@ package com.example.smackchat.Utilities
 
 import java.net.URL
 
-const val BASE_URL = "https://chatachat.herokuapp.com/"
-const val SOCKET_URL = "https://chatachat.herokuapp.com/"
+const val BASE_URL = "https://glacial-beyond-03669.herokuapp.com/"
+const val SOCKET_URL = "https://glacial-beyond-03669.herokuapp.com/"
 const val URL_REGISTER = "${BASE_URL}account/register"
 const val URL_LOGIN = "${BASE_URL}account/login"
 const val URL_CREATE_USER = "${BASE_URL}user/add"
 const val URL_GET_USER = "${BASE_URL}user/byEmail/"
 const val URL_GET_CHANNELS = "${BASE_URL}channel/"
 const val URL_GET_MESSAGES = "${BASE_URL}message/bychannel/"
+
+
 
 
 
